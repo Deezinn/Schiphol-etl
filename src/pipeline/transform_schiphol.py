e16af20ebab225f23446649a1bbf77a706f1a19a
Deezinn <developer.andrelz@gmail.com>
2025-11-18 21:59:48 -0300

    feat: new transform -> aicrafttypes

--- src/pipeline/transform_schiphol.py
+++ src/pipeline/transform_schiphol.py
@@ -31,32 +31,34 @@ class TransformSchiphol(BaseTransform):
         dataframe_flights.to_csv('data/flights.csv')
     
     def _process_destinations(self):
-        new_column_name = {
+        colunas_traduzidas = {
             'country': 'pais',
             'iata': 'codigo_iata',
             'publicName': 'nome_publico',
             'city': 'cidade'
         }
 
-        dataframe_destinations = pd.DataFrame(self.__raw_destinations['destinations'])   
-        dataframe_destinations = dataframe_destinations.rename(columns=new_column_name)
+        valores_invalidos = ['N/A', 'null', 'na', 'undefined', None]
         
-        for coluna in dataframe_destinations.columns:
+        dataframe_destinations = pd.DataFrame(self.__raw_destinations['destinations'])   
+        dataframe_destinations = dataframe_destinations.rename(columns=colunas_traduzidas)
+        dataframe_destinations = dataframe_destinations.fillna('Não informado')
+        dataframe_destinations = dataframe_destinations.replace(valores_invalidos, 'Não informado').fillna('Não informado')
+        
+        for coluna in dataframe_destinations.columns:                
             match coluna:
                 case 'pais' | 'cidade':
                     dataframe_destinations[coluna] = dataframe_destinations[coluna].str.title()
                     dataframe_destinations[coluna] = dataframe_destinations[coluna].str.strip()
-                    dataframe_destinations[coluna] = dataframe_destinations[coluna].fillna('Não informado')
                 case 'codigo_iata':
                     dataframe_destinations[coluna] = dataframe_destinations[coluna].str.upper()
                     dataframe_destinations[coluna] = dataframe_destinations[coluna].str.strip()
-                    dataframe_destinations[coluna] = dataframe_destinations[coluna].fillna('Não informado')
                 case 'nome_publico':
                     dataframe_destinations[coluna] = dataframe_destinations[coluna].apply(get_english)
                     dataframe_destinations[coluna] = dataframe_destinations[coluna].str.strip()
-                    dataframe_destinations[coluna] = dataframe_destinations[coluna].fillna('Não informado')
                 case _:
-                    print(f"Coluna inválida: {coluna}")
+                    pass
+                
         dataframe_destinations.to_csv('data/destinations.csv')
         return dataframe_destinations
         
@@ -66,7 +68,34 @@ class TransformSchiphol(BaseTransform):
     
     def _process_aircraftTypes(self):
         dataframe_aircraftTypes = pd.DataFrame(self.__raw_aircraftTypes['aircraftTypes'])
-        dataframe_aircraftTypes.to_csv('data/aircraftTypes.csv')    
+
+        colunas_traduzidas = {
+            'iataMain' : 'iata_principal', 
+            'iataSub': 'iata_secundario', 
+            'longDescription': 'descricao_longa', 
+            'shortDescription': 'descricao_curta'
+        }
+
+        valores_invalidos = ['N/A', 'null', 'na', 'undefined', None]
+        
+        dataframe_aircraftTypes = dataframe_aircraftTypes.rename(columns=colunas_traduzidas)
+        dataframe_aircraftTypes = dataframe_aircraftTypes.apply(lambda col: col.astype(str).str.strip())
+        dataframe_aircraftTypes = dataframe_aircraftTypes.replace(valores_invalidos, 'Não informado').fillna('Não informado')
+        
+        for coluna in dataframe_aircraftTypes.columns:
+            match coluna:
+                case 'descricao_curta' | 'descricao_longa':
+                    dataframe_aircraftTypes[coluna] = dataframe_aircraftTypes[coluna].str.title()
+                case 'iata_principal' | 'iata_secundario':
+                    dataframe_aircraftTypes[coluna] = dataframe_aircraftTypes[coluna].str.upper()
+                case _:
+                    pass
+                
+        dataframe_aircraftTypes.to_csv('data/aircraftTypes.csv')
+        return dataframe_aircraftTypes
+                    
+            
+
         
         
          
