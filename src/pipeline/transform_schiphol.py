c01e65aaa9d41c45a532d56d43e7598a9f481285
Deezinn <developer.andrelz@gmail.com>
2025-11-19 21:09:05 -0300

    feat: new transform -> airlines

--- src/pipeline/transform_schiphol.py
+++ src/pipeline/transform_schiphol.py
@@ -17,8 +17,12 @@ class TransformSchiphol(BaseTransform):
         if not isinstance(raw_data, dict):
             raise TypeError("Tipo dos dados deveriam vir como dict")
 
-        return cls(raw_flights=raw_data.get('flights') , raw_airlines=raw_data.get('airlines'),
-            raw_destinations=raw_data.get('destinations'), aircraftTypes=raw_data.get('aircraftTypes'))
+        return cls(
+            raw_flights=raw_data.get('flights'),
+            raw_airlines=raw_data.get('airlines'),
+            raw_destinations=raw_data.get('destinations'),
+            aircraftTypes=raw_data.get('aircraftTypes')
+        )
         
     def process_data(self):
         self._process_flights()
@@ -42,20 +46,28 @@ class TransformSchiphol(BaseTransform):
         
         dataframe_destinations = pd.DataFrame(self.__raw_destinations['destinations'])   
         dataframe_destinations = dataframe_destinations.rename(columns=colunas_traduzidas)
-        dataframe_destinations = dataframe_destinations.fillna('Não informado')
         dataframe_destinations = dataframe_destinations.replace(valores_invalidos, 'Não informado').fillna('Não informado')
         
         for coluna in dataframe_destinations.columns:                
             match coluna:
                 case 'pais' | 'cidade':
-                    dataframe_destinations[coluna] = dataframe_destinations[coluna].str.title()
-                    dataframe_destinations[coluna] = dataframe_destinations[coluna].str.strip()
+                    dataframe_destinations[coluna] = (
+                        dataframe_destinations[coluna]
+                        .str.title()
+                        .str.strip()
+                    )
                 case 'codigo_iata':
-                    dataframe_destinations[coluna] = dataframe_destinations[coluna].str.upper()
-                    dataframe_destinations[coluna] = dataframe_destinations[coluna].str.strip()
+                    dataframe_destinations[coluna] = (
+                        dataframe_destinations[coluna]
+                        .str.upper()
+                        .str.strip()
+                    )
                 case 'nome_publico':
-                    dataframe_destinations[coluna] = dataframe_destinations[coluna].apply(get_english)
-                    dataframe_destinations[coluna] = dataframe_destinations[coluna].str.strip()
+                    dataframe_destinations[coluna] = (
+                        dataframe_destinations[coluna]
+                        .apply(get_english)
+                        .str.strip()
+                    )
                 case _:
                     pass
                 
@@ -64,13 +76,59 @@ class TransformSchiphol(BaseTransform):
         
     def _process_airlines(self):
         dataframe_airlines = pd.DataFrame(self.__raw_airlines['airlines'])
+        
+        valores_invalidos = ['N/A', 'null', 'none', 'na', 'undefined', '""', 'None']
+
+        colunas_traduzidas = {
+            'iata': 'codigo_iata',
+            'icao': 'codigo_icao',
+            'nvls': 'nivel',
+            'publicName': 'nome_publico'
+        }
+
+        dataframe_airlines = dataframe_airlines.rename(columns=colunas_traduzidas)
+
+        for coluna in dataframe_airlines.columns:
+            match coluna:
+                case 'nivel':
+                    dataframe_airlines[coluna] = (
+                        dataframe_airlines[coluna]
+                        .replace(valores_invalidos, None)
+                        .astype('Int32')
+                        .fillna(0)
+                    )
+
+                case 'codigo_iata' | 'codigo_icao':
+                    dataframe_airlines[coluna] = (
+                        dataframe_airlines[coluna]
+                        .astype(str)
+                        .replace(valores_invalidos, 'Não informado')
+                        .fillna('Não informado')
+                        .str.upper()
+                        .str.strip()
+                    )
+
+                case 'nome_publico':
+                    dataframe_airlines[coluna] = (
+                        dataframe_airlines[coluna]
+                        .astype(str)
+                        .replace(valores_invalidos, 'Não informado')
+                        .fillna('Não informado')
+                        .str.title()
+                        .str.strip()
+                    )
+
+                case _:
+                    pass
+                
         dataframe_airlines.to_csv('data/airlines.csv')
-    
+        return dataframe_airlines
+            
     def _process_aircraftTypes(self):
         dataframe_aircraftTypes = pd.DataFrame(self.__raw_aircraftTypes['aircraftTypes'])
 
         colunas_traduzidas = {
-            'iataMain' : 'iata_principal', 
+            'iataMain': 'iata_principal', 
             'iataSub': 'iata_secundario', 
             'longDescription': 'descricao_longa', 
             'shortDescription': 'descricao_curta'
@@ -79,8 +137,15 @@ class TransformSchiphol(BaseTransform):
         valores_invalidos = ['N/A', 'null', 'na', 'undefined', None]
         
         dataframe_aircraftTypes = dataframe_aircraftTypes.rename(columns=colunas_traduzidas)
-        dataframe_aircraftTypes = dataframe_aircraftTypes.apply(lambda col: col.astype(str).str.strip())
-        dataframe_aircraftTypes = dataframe_aircraftTypes.replace(valores_invalidos, 'Não informado').fillna('Não informado')
+
+        for coluna in dataframe_aircraftTypes.columns:
+            dataframe_aircraftTypes[coluna] = (
+                dataframe_aircraftTypes[coluna]
+                .replace(valores_invalidos, 'Não informado')
+                .fillna('Não informado')
+                .astype(str)
+                .str.strip()
+            )
         
         for coluna in dataframe_aircraftTypes.columns:
             match coluna:
@@ -93,9 +158,3 @@ class TransformSchiphol(BaseTransform):
                 
         dataframe_aircraftTypes.to_csv('data/aircraftTypes.csv')
         return dataframe_aircraftTypes
-                    
-            
-
-        
-        
-         
